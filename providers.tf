43b25efa81069ff8d12aca61766a98fe6513e126
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-02-25 11:26:14 +0000

    TEST

--- providers.tf
+++ providers.tf
@@ -7,14 +7,14 @@ terraform {
     }
   }
 
-  cloud {
-    organization = "ceniuk-mateusz-org"
-
-    workspaces {
-      name = "stirling-pdf-prod"
-    }
-  }
-}
+#  cloud {
+#    organization = "ceniuk-mateusz-org"
+#
+#    workspaces {
+#      name = "stirling-pdf-prod"
+#    }
+#  }
+#}
 
 provider "azurerm" {
   #subscription_id = var.subscription_id # Read from environment variable or directly from Terraform Cloud
