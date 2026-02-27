15583a3189a5f50c66effcf780f01ed1efe1e6f7
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-02-27 15:50:29 +0000

    delete harcoded test.RG

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -6,12 +6,3 @@ resource "azurerm_resource_group" "rg" {
     environment = "${var.environment}"
   }
 }
-
-resource "azurerm_resource_group" "rg1" {
-  name     = "rg2"
-  location = "POLANDCENTRAL"
-
-  tags = {
-    environment = "${var.environment}"
-  }
-}
