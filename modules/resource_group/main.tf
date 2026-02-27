80153c51df5334e43495dd0e65db527664c7a2e7
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-02-27 15:04:34 +0000

    TEST

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "rg" {
     environment = "${var.environment}"
   }
 }
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg2"
+  Location = var.location
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
