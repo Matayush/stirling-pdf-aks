c375c71ef71fc88dde7b3db5a7701110e67b5164
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-03 13:18:22 +0000

    addin CIDR variable

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,4 +1,8 @@
 variable "resource_group_name" {}  # e.g. "rg-dev"
 variable "location" {}             # e.g. "polandcentral"  
 variable "environment" {}          # e.g. "dev"
-variable "vnet_cidr" { default = "10.0.0.0/16" }  # IP range
+variable "vnet_cidr" {
+    description = "CIDR block for the virtual network"
+    type        = string
+    default = "10.0.0.0/16" 
+}
