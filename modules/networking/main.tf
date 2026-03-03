bcae87c859f37bb501c253504280f9d1b567dabe
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-03 15:59:30 +0000

    var update

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "aks_vnet" {
-  name                = module.resource_group.name
-  location            = module.resource_group.location
+  name                = var.resource_group_name
+  location            = var.resource_group.location
   address_space       = [var.vnet_cidr]
   environment         = var.environment
 
