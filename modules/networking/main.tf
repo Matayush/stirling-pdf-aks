6e65ed2a90a62c4dbfb77a1733a3b37f8f48cc25
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-04 12:22:51 +0000

    var_update

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,24 +1,24 @@
 resource "azurerm_virtual_network" "aks_vnet" {
-  name                = var.resource_group_name
+  name                = "aks_vnet-${var.resource_group_name}"
+  resource_group_name = var.resource_group_name
   location            = var.location
   address_space       = [var.vnet_cidr]
-  environment         = var.environment
 
   tags = {
     environment = "${var.environment}"
   }
 }
 
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "subnet-stirling-${var.environment}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["10.10.0.0/24"]
-}
+#resource "azurerm_subnet" "aks_subnet" {
+#  name                 = "subnet-stirling-${var.environment}"
+#  resource_group_name  = azurerm_resource_group.rg.name
+#  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+#  address_prefixes     = ["10.10.0.0/24"]
+#}
 
-resource "azurerm_subnet" "load_balancer_subnet" {
-  name                 = "subnet-stirling-loadbalancer-${var.environment}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["10.10.1.0/24"]
-}
+#resource "azurerm_subnet" "load_balancer_subnet" {
+#  name                 = "subnet-stirling-loadbalancer-${var.environment}"
+#  resource_group_name  = azurerm_resource_group.rg.name
+#  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+#  address_prefixes     = ["10.10.1.0/24"]
+#}
