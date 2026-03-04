90c5dbe27e2431da7ff137cddc4bc1bd6901ee14
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-04 12:29:02 +0000

    add LB subnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -15,9 +15,10 @@ resource "azurerm_subnet" "aks_subnet" {
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes     = ["10.10.0.0/24"]
 }
-#resource "azurerm_subnet" "load_balancer_subnet" {
-#  name                 = "subnet-stirling-loadbalancer-${var.environment}"
-#  resource_group_name  = azurerm_resource_group.rg.name
-#  virtual_network_name = azurerm_virtual_network.aks_vnet.name
-#  address_prefixes     = ["10.10.1.0/24"]
-#}
+
+resource "azurerm_subnet" "load_balancer_subnet" {
+  name                 = "subnet-loadbalancer-${var.resource_group_name}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
