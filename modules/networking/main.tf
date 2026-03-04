04a44fe3df0854e117b2e6baa5070760c542efc2
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-04 12:26:15 +0000

    add subnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -9,13 +9,12 @@ resource "azurerm_virtual_network" "aks_vnet" {
   }
 }
 
-#resource "azurerm_subnet" "aks_subnet" {
-#  name                 = "subnet-stirling-${var.environment}"
-#  resource_group_name  = azurerm_resource_group.rg.name
-#  virtual_network_name = azurerm_virtual_network.aks_vnet.name
-#  address_prefixes     = ["10.10.0.0/24"]
-#}
-
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "subnet-${var.resource_group_name}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.10.0.0/24"]
+}
 #resource "azurerm_subnet" "load_balancer_subnet" {
 #  name                 = "subnet-stirling-loadbalancer-${var.environment}"
 #  resource_group_name  = azurerm_resource_group.rg.name
