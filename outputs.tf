490ee85a2629d677204d55c3559b13b82c6f2090
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-05 15:59:52 +0000

    add new output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = module.resource_group.location
 }
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
