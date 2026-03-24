caec022935aafdc54ae184f8c04eea2c70badc2b
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-03-24 15:51:24 +0000

    naming fix

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -1,4 +1,4 @@
-output "id" {
-  value       = azurerm_log_analytics_workspace.this.id
+output "log_analytics_workspace_id" {
+  value       = azurerm_log_analytics_workspace.aks.id
   description = "Log Analytics Workspace ID for AKS monitoring"
 }
